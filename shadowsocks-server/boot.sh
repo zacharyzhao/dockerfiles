ac03782d31c3ccf673d106b1d66ba2aca23b6a2b
zachary@zinix.org <zacharyzhao>
2016-10-09 19:14:07 +0800

    Configurable password by environment variable

--- shadowsocks-server/boot.sh
+++ shadowsocks-server/boot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+
+cp /export/servers/shadowsocks-server/etc/config.json.sample /export/servers/shadowsocks-server/etc/config.json
+
+sed -E -i "s/__PRIVATE_PASSWD__/${PRIVATE_PASSWD}/g" /export/servers/shadowsocks-server/etc/config.json
+
+sed -E -i "s/__PUBLIC_PASSWD__/${PUBLIC_PASSWD}/g" /export/servers/shadowsocks-server/etc/config.json
+
 /export/servers/shadowsocks-server/bin/start.sh
 
 sleep 999999d
